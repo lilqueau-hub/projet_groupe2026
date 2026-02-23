3fbb0ba383f097a05d36e7e23a4f51dbfc0afa5b
Diane QUEAU <diane22.queau@aol.com>
2026-02-23 16:05:49 +0100

    commit

--- script.js
+++ script.js
@@ -16,51 +16,11 @@ connectForm.addEventListener('submit', (e)=>{
     const inscriptPseudoValue = inscriptPseudo.value;
     const inscriptEmailValue = inscriptEmail.value;
     const inscriptPasswordValue = inscriptPassword.value;
-    messageError.forEach(error=>{
-        error.classList.add('invisible');
-    });
 
-    if(inscriptPrenomValue.length < 2 || inscriptPrenomValue.length > 10){
-        inscriptPrenomValue.nextElementSibling.classList.remove('invisible');
-    } else if (inscriptNomValue.length < 2 || inscriptNomValue.length > 10){
-        inscriptNomValue.nextElementSibling.classList.remove('invisible');
-    } else if (inscriptPseudoValue.length < 2 || inscriptPseudoValue.length > 10){
-        inscriptPseudoValue.nextElementSibling.classList.remove('invisible');
-    } else if (inscriptEmailValue.length < 2 || inscriptEmailValue.length > 10){
-        inscriptEmailValue.nextElementSibling.classList.remove('invisible');
-    } else if (inscriptPasswordValue.length < 2 || inscriptPasswordValue.length > 10){
-        inscriptPasswordValue.nextElementSibling.classList.remove('invisible');
-    } else {
 
-    }
+    
 })
 
-// function submitInscription() {
-
-// }
-
-
-// class compteGroup {
-//     constructor(pseudo) {
-//         this.pseudo = pseudo;
-//     }
-// }
-async function addUser() {
-    try {
-        const response = await fetch('')
-        const data = await response.json();
-        console.log(data);
-    } catch(err) {
-        console.error("Erreur:", err);
-    }
-}
-addUser();
-
-
-button.addEventListener("click", () => {
-    const inscriptBtn = document.getElementById("btnInscript");
-    localStorage.addUser();
-})
 
 const connectForm = document.getElementById("formulaire-connection");
 const connectPseudo = document.getElementById("pseudo-connect");
@@ -68,22 +28,106 @@ const connectEmail = document.getElementById("email-connect");
 const connectPassword = document.getElementById("password-connect");
 const btnConnect = document.getElementById("connectBtn");
 
-connectForm.addEventListener('', (e)=>{
+connectForm.addEventListener('submit', (e)=>{
     e.preventDefault();
 
     const connectPseudoValue = connectPseudo.value;
     const connectEmailValue = connectEmail.value;
-    const connectPassword = connectPassword.value;
-    messageError.forEach(error=>{
-        error.classList.add('invisible');
+    const connectPasswordValue = connectPassword.value;
+    
+    try {
+        const response = await fetch('http://localhost:8000/api/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
     });
 
-    if(connectPseudoValue.length < 2 || connectPseudoValue.length > 10){
-        connectPseudoValue.nextElementSibling.classList.remove('invisible');
-    } else if (connectEmailValue.length < 2 || connectEmailValue.length > 10){
-        connectEmailValue.nextElementSibling.classList.remove('invisible');
-    } else if (connectPassword.length < 2 || connectPassword.length > 10){
-        connectPassword.nextElementSibling.classList.remove('invisible');
-    } 
+    const result = await response.json();
+    console.log(result);
+
+    } catch(err) {
+        errorDiv.textContent = `Erreur : `
+    }
+
+    
 })
 
+// form.addEventListener('submit', async (e)=> {
+//     e.preventDefault();
+
+//     const cityName = document.getElementById('city').value.trim();
+//     if(!cityName) return;
+    
+//     errorDiv.innerHTML = '';
+//     weatherDiv.innerHTML = '';
+
+//     try {
+//         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${API_KEY}`);
+
+//         if(!response.ok) {
+//             throw new Error(`Erreur HTTP: ${response.status} (${response.statusText}) `);
+//         }
+
+//         const data = await response.json();
+
+//         weatherDiv.innerHTML = `
+//             <h2>Météo à ${data.name} (${data.sys.country})</h2>
+//             <p>Température : ${data.main.temp}°C</p>
+//             <p>Humidité : ${data.main.humidity}%</p>
+//         `
+//     } catch(err) {
+//         errorDiv.textContent = `Erreur : `
+//     }
+// });
+
+const profilDiv = document.getElementById("profilInformation");
+const profilForm = document.getElementById("profilList");
+
+profilForm.addEventListener('submit', async (e)=> {
+    e.preventDefault();
+
+    try {
+        const response = await fetch('');
+        if(!response.ok) {
+            throw new Error(`Erreur HTTP: ${response.status} (${response.statusText}) `);
+        }
+
+        const data = await response.json();
+
+        profilDiv.innerHTML = `è
+            
+        
+        `
+    }
+})
+
+// const form = document.getElementById('weatherForm');
+// const errorDiv = document.getElementById('error');
+// const weatherDiv = document.getElementById('weather');
+
+// const API_KEY = '33b3d10fe54defa518c4da503320deaa';
+
+// form.addEventListener('submit', async (e)=> {
+//     e.preventDefault();
+
+//     const cityName = document.getElementById('city').value.trim();
+//     if(!cityName) return;
+    
+//     errorDiv.innerHTML = '';
+//     weatherDiv.innerHTML = '';
+
+//     try {
+//         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${API_KEY}`);
+
+        
+//         const data = await response.json();
+
+//         weatherDiv.innerHTML = `
+//             <h2>Météo à ${data.name} (${data.sys.country})</h2>
+//             <p>Température : ${data.main.temp}°C</p>
+//             <p>Humidité : ${data.main.humidity}%</p>
+//         `
+//     } catch(err) {
+//         errorDiv.textContent = `Erreur : `
+//     }
+// });
